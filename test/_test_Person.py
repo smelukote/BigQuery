b6964a85d5ee9c795aa6e98def67dbef4f256627
smelukote <smelukote@gmail.com>
2022-01-05 16:35:03 -0800

    Added unittest

--- test/_test_Person.py
+++ test/_test_Person.py
@@ -1,20 +1,15 @@
 import unittest
 
-class Person:
-    name = []
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
 
-    def set_name(self, user_name):
-        self.name.append(user_name)
-        return len(self.name) - 1
-
-    def get_name(self, user_id):
-        if user_id >= len(self.name):
-            return 'There is no such user'
-        else:
-            return self.name[user_id]
-
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
 
 if __name__ == '__main__':
-    person = Person()
-    print('User Abbas has been added with id ', person.set_name('Abbas'))
-    print('User associated with id 0 is ', person.get_name(0))
+    unittest.main()
